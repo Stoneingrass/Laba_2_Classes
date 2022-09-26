617e1a57cb5952561ae068ddf80de3f0dee22dd7
Illya <svirillya@gmail.com>
2022-09-26 20:42:00 +0300

    Initial commit

---
+++ src/laba2/Item.java
@@ -0,0 +1,64 @@
+package laba2;
+
+/* Describes item object has a name and a price */
+public class Item {
+    //fields
+    private String name;    //item name
+    private float price;    //item price
+
+    //constructor
+    public Item (String name, float price) {
+        this.name=name;
+        this.price=price;
+        nullingPriceIfPriceIsNegative();
+    }
+
+
+    //getters
+    public String getName() {
+        return name;
+    }
+    public float getPrice() {
+        return price;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name=name;
+    }
+    public void setPrice(float price) {
+        this.price=price;
+        nullingPriceIfPriceIsNegative();
+    }
+
+
+    //public methods (interface)
+
+    //decrease in price by a certain percentage
+    public void decreasePrice(float percent) {
+        decrease(percent);
+    }
+    private void decrease(float percent) {
+        price-=price*percent/100;
+        nullingPriceIfPriceIsNegative();
+    }
+
+    //increase in price by a certain percentage
+    public void increasePrice(float percent) {
+        increase(percent);
+    }
+    private void increase(float percent) {
+        price+=price*percent/100;
+        nullingPriceIfPriceIsNegative();
+    }
+
+
+    //private methods
+
+    //price negativity test
+    private void nullingPriceIfPriceIsNegative() {
+        if (price<0) {
+            price=0;
+        }
+    }
+}
---
+++ src/laba2/Main.java
@@ -0,0 +1,63 @@
+package laba2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Cart basket;
+
+        basket = inputItems();
+
+        System.out.println("\nThe cart:");
+        outputCart(basket);
+
+        System.out.println("Sum of prices of all items in the cart: " + basket.sumItemPrice());
+
+        System.out.println("\nIncreasing a price by 15%: ");
+        basket.increaseAllItemPrice(15);
+        outputCart(basket);
+        System.out.println("Sum of prices of all items in the cart: " + basket.sumItemPrice());
+
+        System.out.println("\nDecreasing a price by 30%: ");
+        basket.decreaseAllItemPrice(30);
+        outputCart(basket);
+        System.out.println("Sum of prices of all items in the cart: " + basket.sumItemPrice());
+    }
+
+    //adding items to cart
+    public static Cart inputItems() {
+        int itemNumber;
+        Cart basket;
+        Scanner nameScanner = new Scanner(System.in);
+        Scanner numberScanner = new Scanner(System.in);
+
+        //input number of items in the cart
+        do {
+            System.out.println("Input items number in the cart (>0):");
+            itemNumber = numberScanner.nextInt();
+            if (itemNumber <= 0) System.out.println("Please, input a number more than 0.");
+        } while (itemNumber <= 0);
+
+        //cart initialisation
+        basket = new Cart(itemNumber);
+
+        //cart filling by items
+        for (int i = 0; i < itemNumber; i++) {
+            System.out.println("\nInput name of item #" + (i + 1));
+            String name = nameScanner.nextLine();
+            System.out.println("Input price of item #" + (i + 1));
+            float price = numberScanner.nextFloat();
+
+            basket.addItem(new Item(name, price));
+        }
+
+        return basket;
+    }
+
+    //outputting of the cart
+    public static void outputCart (Cart basket) {
+        for (int i=0; i<basket.getItemNumber(); i++) {
+            System.out.println("Item #" + (i + 1) + ": " + basket.getStack()[i].getName() + " - " + basket.getStack()[i].getPrice());
+        }
+    }
+}
